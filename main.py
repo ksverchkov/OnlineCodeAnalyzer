d35fc805f4255e24e326368f8d843dde106f9cf5
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2023-02-17 07:24:39 +0300

    Edited debug, that crashes all

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ def index():
     return render_template('index.html', title=title)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
